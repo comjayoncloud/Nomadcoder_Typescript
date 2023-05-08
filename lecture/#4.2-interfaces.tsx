226d787bfeaad07e797b24662792059df0868c35
jaewoo <comjayoncloud@gmail.com>
2023-05-08 16:16:07 +0900

    lecture 4.1 : interface

---
+++ lecture/#4.2-interfaces.tsx
@@ -0,0 +1,32 @@
+// interface User {
+//   name: String;
+// }
+
+/** interface를 type으로 바꿧을때 player의 모양 */
+// interface Player extends User {}
+
+// type Player2 = User & {};
+
+// const jaewoo: Player = {
+//   name: "jaewoo",
+// };
+
+// jaewoo.name = "lalalala";
+
+interface User {
+  name: string;
+}
+
+interface User {
+  lastname: string;
+}
+
+interface User {
+  health: number;
+}
+
+const jaewoo: User = {
+  name: "jaewoo",
+  lastname: "jeong",
+  health: 10,
+};
